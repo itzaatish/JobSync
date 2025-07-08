f1693aaa4bcdc02c32c3a7a05a20075acef0be75
itzaatish <byjsr71@gmail.com>
2025-07-08 19:10:07 +0530

    Changed base for commit

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -3,12 +3,11 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import { HashRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode> 
-    <HashRouter>
+    
       <App />
-    </HashRouter>
+    
   </StrictMode>,
 )
--- Client/vite.config.js
+++ Client/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: 'JobSync', // replace with your repo name
+  base: '/JobSync/', // replace with your repo name
 })
